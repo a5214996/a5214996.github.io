5ce857ffb176b24309c28ee7a9af5231329497bc
q <>
2019-07-27 14:18:30 -0700

    1

--- y/javascript.js
+++ y/javascript.js
@@ -14,7 +14,7 @@ function refreshChat(){
 
 function handleVisibilityChange() {
   if (document.hidden)
-    document.title = "Hot Dudes";
+    document.title = "HotDudes";
   else 
     document.title = "Do not share";
 }
