963a2193ecb17ec9b9c8a5baeb8b3167c35b68dc
q <>
2019-10-13 17:01:55 -0700

    1

--- javascript.js
+++ javascript.js
@@ -4,8 +4,8 @@ var stop = "modpoll";
 var wait = "";
 var mobile = false;
 
-$('.d').text("10%");
-$('#ds').text("Tman"); 
+$('.d').text("30%");
+$('#ds').text("Nivke, Tman"); 
 
 function refreshChat(){
 	var chat = document.getElementById("chat");
