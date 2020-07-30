87a75c310c0f9c463f383309c3eb8eadffe04628
q <>
2020-07-30 12:39:09 -0700

    1

--- brad/javascript.js
+++ brad/javascript.js
@@ -277,7 +277,7 @@ function GetPlayerEmbed(o, c) {
 		<a href="#" onclick="updateServer('e1');return false;" style="font-size:30px;color:#FFF"><b>East 1</b></a> |
 		<a href="#" onclick="updateServer('e2');return false;" style="font-size:30px;color:#FFF"><b>East 2</b></a> |
 		<a href="#" onclick="updateServer('eu');return false;" style="font-size:30px;color:#FFF"><b>Europe</b></a> |
-		<a href="#" onclick="updateServer('sea');return false;" style="font-size:30px;color:#FFF"><b>SEA</b></a></br>
+		<a href="#" onclick="updateServer('sea');return false;" style="font-size:30px;color:#FFF"><b>SEA</b></a></br><br/>
 		<a href="https://www.patreon.com/join/angelthump"><img src="https://i.imgur.com/nxzeGa4.png" alt="Angelthump"></a></h1>`;
 	}	
 }
