39d03fafac1d59ad27ecc0869c1199ad595a34d2
q <>
2020-07-30 18:38:07 -0700

    1

--- brad/javascript.js
+++ brad/javascript.js
@@ -275,10 +275,10 @@ function GetPlayerEmbed(o, c) {
 		Select a server:<br/>
 		<a href="#" onclick="updateServer('e1');return false;" style="font-size:30px;color:#FFF"><b>US East 1</b></a> |
 		<a href="#" onclick="updateServer('e2');return false;" style="font-size:30px;color:#FFF"><b>US East 2</b></a> |
-		<a href="#" onclick="updateServer('w');return false;" style="font-size:30px;color:#FFF"><b>US West</b></a> |
-		<a href="#" onclick="updateServer('eu1');return false;" style="font-size:30px;color:#FFF"><b>Europe 1</b></a> |
-		<a href="#" onclick="updateServer('eu2');return false;" style="font-size:30px;color:#FFF"><b>Europe 2 </b></a> |
-		<a href="#" onclick="updateServer('eu3');return false;" style="font-size:30px;color:#FFF"><b>Europe 3</b></a> |
+		<a href="#" onclick="updateServer('w');return false;" style="font-size:30px;color:#FFF"><b>US West</b></a></br>
+		<a href="#" onclick="updateServer('eu1');return false;" style="font-size:30px;color:#FFF"><b>EU 1</b></a> |
+		<a href="#" onclick="updateServer('eu2');return false;" style="font-size:30px;color:#FFF"><b>EU 2 </b></a> |
+		<a href="#" onclick="updateServer('eu3');return false;" style="font-size:30px;color:#FFF"><b>EU 3</b></a> |
 		<a href="#" onclick="updateServer('ru');return false;" style="font-size:30px;color:#FFF"><b>Russia</b></a> |
 		<a href="#" onclick="updateServer('sea');return false;" style="font-size:30px;color:#FFF"><b>SEA</b></a><br/></br>
 		<!--<a href="https://www.patreon.com/join/angelthump" target="_blank"><img src="https://i.imgur.com/GXERebi.png" alt="Angelthump"></a>--></h1>`;
