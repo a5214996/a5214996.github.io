138f89b3fdde4880061bc5d4eafe8cab93dbbdf0
q <41808470+a5214996@users.noreply.github.com>
2025-04-06 09:58:50 -0700

    a

--- rob/javascript.js
+++ rob/javascript.js
@@ -214,6 +214,10 @@ function GetPlayerEmbed(o, c) {
 	
      if(c.indexOf("1-edge") > -1)
           c = updateC(c);
+     }
+     if (o == "offline") {
+          document.getElementById("b").style.display = "none";
+     }
 
 	switch (o) {
 	case 'warning':
@@ -221,7 +225,7 @@ function GetPlayerEmbed(o, c) {
 	case 'refresh':
 		return '<script>location.reload();</script>';
 	case 'offline':
-		return '<div id=b><span class="noselect fff"><a href="https://discord.gg/quackpack" target="_blank"><img src="discord.png" style="height:32px"></a></div>';
+		return '<div id=o><a href="https://discord.gg/quackpack" target="_blank"><img src="discord.png" style="height:32px"></a></div>';
 	case 'ustream':
 		return '<iframe src="https://www.ustream.tv/embed/' + c + '?html5ui&showtitle=false" width=100% height=100% resizable=true id=stream frameborder=0 scrolling=no allowtransparency=true allowfullscreen></iframe>';
 	case 'youtube':
