6a1cb59d883b3f5b6c3d1363e0bdbec09d1a0c7b
q <41808470+a5214996@users.noreply.github.com>
2025-04-19 06:34:49 -0700

    a

--- james/javascript.js
+++ james/javascript.js
@@ -1,5 +1,5 @@
 ﻿const channel = "lal420";
-const stov = "modvideo";
+const lastModifiedKey = "modvideo";
 
 document.addEventListener("visibilitychange", () => {
   document.title = document.hidden ? "Hot Dudes" : "Do not share";
@@ -14,9 +14,9 @@ function updateVideo() {
     url: `video.json?${Date.now()}`,
     type: "GET",
     dataType: "json",
-    success: (response, status, xhr) => {
-      SetPlayer(response.host, response.channel);
-      sessionStorage.setItem(stov, xhr.getResponseHeader("Last-Modified"));
+    success: (response, _, xhr) => {
+      setPlayer(response.host, response.channel);
+      sessionStorage.setItem(lastModifiedKey, xhr.getResponseHeader("Last-Modified"));
     },
     error: (xhr) => {
       try {
@@ -29,83 +29,62 @@ function updateVideo() {
   });
 }
 
-function hideChat() {
+function toggleChat() {
   const $side = $('#side');
+  const isVisible = $side.is(":visible");
+
   $side.toggle();
+  $("#chatvo").text(isVisible ? "show chat" : "hide chat");
+  $("#main").css("width", isVisible ? "100%" : "calc(100% - 283px)");
 
-  if ($side.is(":visible")) {
-    $("#chatvo").html("hide chat");
-    $("#main").css("width", "calc(100% - 283px)");
-  } else {
-    $("#chatvo").html("show chat");
-    $("#main").css("width", "100%");
-  }
-
-  SetChat();
+  setChat();
   updateVideo();
 }
 
-function ajax30sInterval() {
+function checkForVideoUpdate() {
   $.ajax({
     url: `video.json?${Date.now()}`,
     type: "HEAD",
     success: (_, __, xhr) => {
-      if (xhr.getResponseHeader("Last-Modified") !== sessionStorage.getItem(stov)) {
+      if (xhr.getResponseHeader("Last-Modified") !== sessionStorage.getItem(lastModifiedKey)) {
         updateVideo();
       }
     }
   });
 }
 
-function SetPlayer(o, c) {
-  if (!c) {
-    o = "offline";
-  } else if (
-    typeof Storage !== "undefined" &&
-    /streamify|broadcast|sawlive|castamp|dot|vidi|ustream|widestream|dailymotion|ssh/.test(o) &&
-    window.innerWidth > 600 &&
-    !localStorage.getItem("warning")
-  ) {
-    o = "warning";
-    localStorage.setItem("warning", "1");
-  }
-
-  $('#video-wrapper').html(GetPlayerEmbed(o, c));
+function setPlayer(host, channel) {
+  const playerType = !channel ? "offline" : host;
+  $('#video-wrapper').html(getPlayerEmbed(playerType, channel));
 }
 
 function getRes() {
   const sideWidth = $('#side').is(':visible') ? $('#side').width() : 0;
-  const w = Math.floor(window.innerWidth - sideWidth);
-  const h = Math.floor(w * 0.5625);
-  return { w, h };
+  const width = Math.floor(window.innerWidth - sideWidth);
+  return { width, height: Math.floor(width * 0.5625) };
 }
 
-function GetPlayerEmbed(o, c) {
-  const { w, h } = getRes();
+function getPlayerEmbed(host, channel) {
+  const { width, height } = getRes();
 
-  if (c.includes("1-edge")) c = updateC(c);
-  if (o === "offline") document.getElementById("b").style.display = "none";
+  if (channel && channel.includes("1-edge")) channel = updateChannel(channel);
 
-  switch (o) {
-    case "warning":
-      return `<h1 style="font-family: Helvetica,Tahoma;font-size:36px;text-align:center;position:absolute;top:50%;left:50%;transform:translate(-50%, -50%);color:white;">
-        PLEASE DO NOT SHARE<br/><br/>
-        <a href="https://www.ublock.org" style="color:#FB2" target="_blank">USE ADBLOCK</a><br/><br/>
-        <a href="#" onclick="updateVideo();return false;" style="font-size:50px;color:#FB2"><b>OK</b></a>
-      </h1>`;
+  if (host === "offline") document.getElementById("b").style.display = "none";
+
+  switch (host) {
     case "offline":
-      return '<div id="o"><a href="https://discord.gg/quackpack" target="_blank"><img src="discord.png" style="height:32px"></a></div>';
+      return getOfflineEmbed();
     case "youtube":
-      return `<iframe src="https://www.youtube.com/embed/${c}?autoplay=1" width="100%" height="100%" id="stream" frameborder="0" allowfullscreen></iframe>`;
+      return `<iframe src="https://www.youtube.com/embed/${channel}?autoplay=1" width="100%" height="100%" id="stream" frameborder="0" allowfullscreen></iframe>`;
     case "clappr":
       return `<div id="player"></div><script>
         new Clappr.Player({
-          source: "${c}",
+          source: "${channel}",
           mute: true,
           autoPlay: true,
           plugins: [DashShakaPlayback, QualitySelector],
           parentId: "#player",
-          height: ${h},
+          height: ${height},
           width: "100%"
         });
       </script>`;
@@ -114,33 +93,41 @@ function GetPlayerEmbed(o, c) {
         new Twitch.Player("twitch-player", {
           width: "100%",
           height: "100%",
-          channel: "${c}"
+          channel: "${channel}"
         }).setVolume(0.0);
       </script>`;
-	case "angels":
-      return `<h1 style="font-size:40px;text-align:center;position:absolute;top:50%;left:50%;transform:translate(-50%, -50%);color:white;">
-        Select a server:<br/>
-        <a href="#" onclick="updateServer('e');return false;" style="font-size:30px;color:#FFF"><b>US East</b></a> |
-        <a href="#" onclick="updateServer('w');return false;" style="font-size:30px;color:#FFF"><b>US West</b></a><br/>
-        <a href="#" onclick="updateServer('eu1');return false;" style="font-size:30px;color:#FFF"><b>EU 1</b></a> |
-        <a href="#" onclick="updateServer('eu2');return false;" style="font-size:30px;color:#FFF"><b>EU 2</b></a> |
-        <a href="#" onclick="updateServer('sea');return false;" style="font-size:30px;color:#FFF"><b>SEA</b></a><br/>
-        <span style="font-size:20px">Try a different server if you're lagging</span>
-      </h1>`;
+    case "angels":
+      return getServerSelectionEmbed();
     default:
-      return `<iframe src="${c}" width="100%" height="100%" id="stream" frameborder="0" allowfullscreen></iframe>`;
+      return `<iframe src="${channel}" width="100%" height="100%" id="stream" frameborder="0" allowfullscreen></iframe>`;
   }
 }
 
-function updateC(c) {
+function getOfflineEmbed() {
+  return '<div id="o"><a href="https://discord.gg/quackpack" target="_blank"><img src="discord.png" style="height:32px"></a></div>';
+}
+
+function getServerSelectionEmbed() {
+  return `<h1 style="font-size:40px;text-align:center;position:absolute;top:50%;left:50%;transform:translate(-50%, -50%);color:white;">
+    Select a server:<br/>
+    <a href="#" onclick="updateServer('e');return false;" style="font-size:30px;color:#FFF"><b>US East</b></a> |
+    <a href="#" onclick="updateServer('w');return false;" style="font-size:30px;color:#FFF"><b>US West</b></a><br/>
+    <a href="#" onclick="updateServer('eu1');return false;" style="font-size:30px;color:#FFF"><b>EU 1</b></a> |
+    <a href="#" onclick="updateServer('eu2');return false;" style="font-size:30px;color:#FFF"><b>EU 2</b></a> |
+    <a href="#" onclick="updateServer('sea');return false;" style="font-size:30px;color:#FFF"><b>SEA</b></a><br/>
+    <span style="font-size:20px">Try a different server if you're lagging</span>
+  </h1>`;
+}
+
+function updateChannel(channel) {
   const con = Date.now();
-  if (c.includes("1-edge") && mobile) {
-    const ch = c.split("/")[4].split(".")[0];
-    c = `${c.split('tv')[0]}tv/hls/${ch}/index.m3u8`;
+  if (channel.includes("1-edge") && mobile) {
+    const ch = channel.split("/")[4].split(".")[0];
+    channel = `${channel.split('tv')[0]}tv/hls/${ch}/index.m3u8`;
   }
-  if (c.includes("1-edge")) c += `?token=public&con=${con}`;
-  if (c.includes("east")) c = c.replace(/[4]/, Math.floor(Math.random() * 2) + 4);
-  return c;
+  if (channel.includes("1-edge")) channel += `?token=public&con=${con}`;
+  if (channel.includes("east")) channel = channel.replace(/[4]/, Math.floor(Math.random() * 2) + 4);
+  return channel;
 }
 
 function updateServer(region) {
@@ -150,7 +137,7 @@ function updateServer(region) {
     dataType: "json",
     success: (response, _, xhr) => {
       $('#video-wrapper').html(setServer(response.host, response.channel, region));
-      sessionStorage.setItem(stov, xhr.getResponseHeader("Last-Modified"));
+      sessionStorage.setItem(lastModifiedKey, xhr.getResponseHeader("Last-Modified"));
     },
     error: (xhr) => {
       try {
@@ -163,49 +150,46 @@ function updateServer(region) {
   });
 }
 
-function setServer(o, c, s) {
-  const { h } = getRes();
-  if (o === "angels") {
-    const map = {
+function setServer(host, channel, region) {
+  const { height } = getRes();
+  if (host === "angels") {
+    const serverMap = {
       w: "sfo1",
       e: "nyc1",
       eu1: "fra1",
       eu2: "ams1",
       sea: "sgp1"
     };
-    c = c.replace("#", map[s]);
+    channel = channel.replace("#", serverMap[region]);
     return `<div id="player"></div><script>
       new Clappr.Player({
-        source: "${c}",
+        source: "${channel}",
         mute: true,
         autoPlay: true,
         parentId: "#player",
-        height: ${h},
+        height: ${height},
         width: "100%"
       });
     </script>`;
   }
 }
 
-function SetChat() {
+function setChat() {
   const mode = $("#chatvo").text().includes("show") ? "hide" : "night";
-  $('#chat-container').html(GetChatEmbed(channel, mode));
+  $('#chat-container').html(getChatEmbed(channel, mode));
 }
 
-function GetChatEmbed(channel, mode) {
-    switch (mode) {
-        case 'night':
-            return '<script id="cid0020000080393759078" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 100%;">{"handle":"' + channel + '","arch":"js","styles":{"a":"000000","b":100,"c":"444444","d":"FFFFFF","e":"000000","f":100,"g":"444444","h":"000000","i":100,"j":"444444","k":"444444","l":"000000","m":"444444","n":"444444","p":11,"q":"000000","r":0,"t":0,"v":0,"ab":1,"sbc":"444444","sba":100,"surl":0,"allowpm":1,"v":0,"w":0,"showhdr":0,"showx":0}}</script>';
-		/*return '<iframe src="https://www.twitch.tv/lal420/chat?darkpopout=" frameborder="0" scrolling="no" height="100%" width="100%"></iframe>'*/
-        case 'day':
-            return '<script id="cid0020000080393759078" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 100%;">{"handle":"' + channel + '","arch":"js","styles":{"a":"3c2569","b":100,"c":"FFFFFF","d":"FFFFFF","e":"FFFFFF","f":100,"g":"000000","h":"FFFFFF","i":100,"j":"000000","k":"000000","l":"3c2569","m":"FFFFFF","n":"FFFFFF","p":11,"q":"000000","r":0,"t":0,"v":0,"ab":0,"sbc":"3c2569","sba":100,"surl":0,"allowpm":1,"v":0,"w":0,"showhdr":0,"showx":0}}</script>';
-        case 'hide':
-            return ''
-    }
+function getChatEmbed(channel, mode) {
+  if (mode === 'night') {
+    return `<script id="cid0020000080393759078" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 100%;">` +
+      `{"handle":"${channel}","arch":"js","styles":{"a":"000000","b":100,"c":"444444","d":"FFFFFF","e":"000000","f":100,"g":"444444","h":"000000","i":100,"j":"444444","k":"444444","l":"000000","m":"444444","n":"444444","p":11,"q":"000000","r":0,"t":0,"v":0,"ab":1,"sbc":"444444","sba":100,"surl":0,"allowpm":1,"v":0,"w":0,"showhdr":0,"showx":0}}` +
+      '</script>';
+  }
+  return ''; 
 }
 
 $(document).ready(() => {
-  SetChat();
+  setChat();
   updateVideo();
-  setInterval(ajax30sInterval, 30000);
+  setInterval(checkForVideoUpdate, 30000);
 });
