f1f2b47834aa3c773608e6afea7fb2d44ae8cbdd
q <41808470+a5214996@users.noreply.github.com>
2026-06-30 14:46:46 -0700

    1

--- assets/javascript.js
+++ assets/javascript.js
@@ -76,7 +76,7 @@ function getPlayerEmbed(host, channel) {
       return getServerSelectionEmbed();
     case "urlparam": {
       const slug = window.location.search.slice(1); // strips the '?'
-      if (!slug) return getOfflineEmbed();
+      if (!slug) return '<div id="o">url parameter missing?</div>';
       const url = `${channel}${getStreamDate()}/${slug}`;
       return `<iframe src="${url}" width="100%" height="100%" id="stream" frameborder="0" allowfullscreen></iframe>`;
     }
